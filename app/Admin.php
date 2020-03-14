14d5de7b58755983787cc8f549aa1697145ccf71
john joseph florina <devjosephflorina@gmail.com>
2020-03-14 16:50:40 +0800

    first  and have  lte admindashboard and admin login

---
+++ app/Admin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use illuminate\Notifications\Notifiable;
+use illuminate\Foundation\Auth\User as Authenticatable;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Model
+{
+    //
+}
